e733b78024971a83b195ca89f8262c0ee7e19b37
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2017-03-12 13:33:15 -0400

    Update build.sh to make sure that the OUD installation ZIP is present
    More work on this to come.

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+ZIPFILE=ofm_oud_generic_11.1.2.3.0_disk1_1of1.zip
+
+
+if [ ! -e $ZIPFILE ]; then
+  echo "Copy (or hard link) $ZIPFILE into this directory"
+  exit -1
+fi
+
 docker build -t oracle/oud:11.1.2.3.0 .
